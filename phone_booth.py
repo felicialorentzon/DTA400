a5c411a2a15ba48d0d6327724ebc2ea0130b564b
Hampus Avekvist <ha@leissner.se>
2024-10-18 19:42:35 +0200

    refactor: move stuff into a main-if-statement

--- phone_booth.py
+++ phone_booth.py
@@ -14,8 +14,11 @@ T_INTER = 8           # The interval a person arrives
 SIM_TIME = 3000        # Simulation time in minutes
 # fmt: on
 
+
 class PhoneBooth:
-    def __init__(self, env: simpy.Environment, phone_service, num_phones: int, call_time: int):
+    def __init__(
+        self, env: simpy.Environment, phone_service, num_phones: int, call_time: int
+    ):
         self.env = env
         self.phone_service = phone_service
         self.num_phones = num_phones
@@ -25,8 +28,11 @@ class PhoneBooth:
     def call(self, person):
         yield self.env.process(self.phone_service.call(person))
 
+
 class Person:
-    def __init__(self, env: simpy.Environment, phonebooth: PhoneBooth, name: int, call_time: int):
+    def __init__(
+        self, env: simpy.Environment, phonebooth: PhoneBooth, name: int, call_time: int
+    ):
         self.env = env
         self.phonebooth = phonebooth
         self.name = name
@@ -58,6 +64,7 @@ class Person:
             self.finished = env.now
             self.insufficient_funds = self.finished == self.access
 
+
 class PhoneService:
     def __init__(self, env: simpy.Environment):
         self.env = env
@@ -65,48 +72,60 @@ class PhoneService:
         self.channels = simpy.Resource(env, NUM_CHANNELS)
 
     def call(self, person: Person):
-        with self.channels.request() as caller:
+        with self.channels.request() as _caller:
             self.current_channels += 1
             yield env.timeout(CALL_SETUP_TIME)
 
             if person.call_time <= 0:
                 raise Exception
 
-            if self.current_channels > CALL_DROP_AMOUNT and random.random() > CALL_DROP_RATE:
+            if (
+                self.current_channels > CALL_DROP_AMOUNT
+                and random.random() > CALL_DROP_RATE
+            ):
                 raise RuntimeError
-            
-            with self.channels.request() as called:
+
+            with self.channels.request() as _called:
                 self.current_channels += 1
-                start = env.now
+                _start = env.now
                 yield env.timeout(person.call_time)
-                end = env.now
+                _end = env.now
                 # print(f"Person {person.name} talked for {end - start:.2f} minutes at {env.now:.2f}")
             self.current_channels -= 1
         self.current_channels -= 1
-            
+
+
 def setup(env, t_inter):
     for person in persons:
         yield env.timeout(random.randint(t_inter - 8, t_inter + 8))
         env.process(person.process())
 
-# Create an environment 
-env = simpy.Environment()
 
-phone_service = PhoneService(env)
+if __name__ == "__main__":
+    # Create an environment
+    env = simpy.Environment()
 
-# Create the phone booths
-phonebooth = PhoneBooth(env, phone_service, NUM_PHONES, MAX_CALL_TIME)
+    phone_service = PhoneService(env)
 
-persons = [Person(env, phonebooth, name, random.randint(0, MAX_CALL_TIME)) for name in range(NUM_PERSONS)]
+    # Create the phone booths
+    phonebooth = PhoneBooth(env, phone_service, NUM_PHONES, MAX_CALL_TIME)
 
-# Setup and start the simulation
-random.seed(RANDOM_SEED)  # This helps to reproduce the results
+    persons = [
+        Person(env, phonebooth, name, random.randint(0, MAX_CALL_TIME))
+        for name in range(NUM_PERSONS)
+    ]
 
-# Start the setup process
-env.process(setup(env, T_INTER))
+    # Setup and start the simulation
+    random.seed(RANDOM_SEED)  # This helps to reproduce the results
 
-# Execute!
-env.run(until=SIM_TIME)
+    # Start the setup process
+    env.process(setup(env, T_INTER))
 
-for person in persons:
-    print(f"{person.name:5} | {person.arrival:5.2f} | {person.access:5.2f} | {person.finished:5.2f} | {person.insufficient_funds}")
+    # Execute!
+    env.run(until=SIM_TIME)
+
+    for person in persons:
+        print(
+            f"{person.name:5} | {person.arrival:5.2f} | {person.access:5.2f} | {person.finished:5.2f} | {person.insufficient_funds}"
+        )
+
