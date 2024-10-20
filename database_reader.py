9df257acb05643dad70ba2be748327ac2dbe5969
Hampus Avekvist <ha@leissner.se>
2024-10-20 19:41:49 +0200

    feat: more features and graphs

--- database_reader.py
+++ database_reader.py
@@ -1,14 +1,60 @@
 import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
+
+WITH_DROP = True
 
 if __name__ == "__main__":
+    plt.close("all")
+
+    person_statistics = None
+    time_series = None
+
     with sqlite3.connect("statistics.db", autocommit=True) as db:
-        cursor = db.cursor()
+        person_statistics = pd.read_sql("SELECT * FROM person_statistics", db)
+        time_series = pd.read_sql("SELECT * FROM time_series", db)
 
-        cursor.execute("SELECT * FROM person_statistics")
-        statistics = cursor.fetchall()
-        cursor.close()
+    title = " with drop" if WITH_DROP else ""
+    filename = "_with_drop" if WITH_DROP else ""
 
-        print(f"{"ID":<5} {"Starting funds":<15} {"Arrival":<9} {"Access":<8} {"Call start":<12} {"Finished":<10} {"Insufficient funds":<18}")
-        for person in statistics:
-            (id, start_funds, arrival, access, call_start, finished, insufficient_funds) = person
-            print(f"{id:<5} {start_funds:<15} {arrival:<9.2f} {access:<8.2f} {call_start:<12.2f} {finished:<10.2f} {insufficient_funds:<18}")
+    df = person_statistics
+    queue_duration = df["access"] - df["arrival"]
+    plt.figure()
+    queue_duration.plot()
+    plt.xlabel("Number of people")
+    plt.ylabel("Time waiting in queue")
+    plt.title(f"Time waiting in queue{title}")
+    plt.savefig(f"wait_time{filename}.png", dpi=600)
+
+    df = person_statistics
+    queue_duration = (df["access"] - df["arrival"]) / df["id"]
+    df = queue_duration.cumsum()
+    plt.figure()
+    queue_duration.plot()
+    plt.xlabel("Number of people")
+    plt.ylabel("Cumulative summation of time waiting in queue")
+    plt.title(f"Cumulative summation of time waiting in queue{title}")
+    plt.savefig(f"cumsum_wait_time{filename}.png", dpi=600)
+
+    print(queue_duration.mean(axis=0))
+
+    df = time_series
+    df = df[["timestamp", "queue_size"]]
+    df.set_index("timestamp", inplace=True)
+    plt.figure()
+    df.plot(figsize=(16, 4))
+    plt.xlabel("Timestamp (minutes)")
+    plt.ylabel("People in queue")
+    plt.title(f"Queue size{title}")
+    plt.savefig(f"queue_size{filename}.png", dpi=600)
+
+    if WITH_DROP:
+        df = time_series
+        df = df[["timestamp", "total_drops"]]
+        df.set_index("timestamp", inplace=True)
+        plt.figure()
+        df.plot()
+        plt.xlabel("Timestamp (minutes)")
+        plt.ylabel("Channels dropped")
+        plt.title("Channels dropped")
+        plt.savefig("channels_dropped.png", dpi=600)
--- phone_booth.py
+++ phone_booth.py
@@ -4,15 +4,57 @@ import simpy
 
 # fmt: off
 RANDOM_SEED = 42
-NUM_CHANNELS = 99999  # Number of channels in the phone service
-NUM_PHONES = 200      # Number of phones in the phone booth
-MAX_CALL_TIME = 45    # Duration of a call and the minutes the person can afford to call
-CALL_SETUP_TIME = 0.5 # The time to authenticate and dial a peer
-NUM_PERSONS = 400     # Number of persons in the group
-CALL_DROP_RATE = 0.9  # The probability that calls are droped when high load
-CALL_DROP_AMOUNT = 5  # The number of active calls required before service starts failing
-TIME_INTERVAL = 8     # The interval a person arrives
+NUM_CHANNELS = 99999   # Number of channels in the phone service
+NUM_PHONES = 20        # Number of phones in the phone booth
+MAX_CALL_TIME = 80     # Duration of a call and the minutes the person can afford to call
+CALL_SETUP_TIME = 0.5  # The timestamp to authenticate and dial a peer
+NUM_PERSONS = 600      # Number of persons in the group
+CALL_DROP_RATE = 0.95  # The probability that calls are droped when high load
+CALL_DROP_AMOUNT = 34  # The number of active calls required before service starts failing
+TIME_INTERVAL = 2      # The interval a person arrives
+DEBUG = False
 # fmt: on
+
+
+queue_size = 0
+active_channels = 0
+total_channels = 0
+active_calls = 0
+total_calls = 0
+total_drops = 0
+previous_timestamp = -1
+
+
+def printer(message: str):
+    if DEBUG:
+        print(message)
+
+
+def time_series(db: sqlite3.Cursor, timestamp):
+    global queue_size
+    global active_channels
+    global total_channels
+    global active_calls
+    global total_calls
+    global total_drops
+    global previous_timestamp
+
+    if timestamp <= previous_timestamp:
+        return
+    previous_timestamp = timestamp
+
+    db.execute(
+        "INSERT INTO time_series VALUES (?, ?, ?, ?, ?, ?, ?)",
+        (
+            timestamp,
+            queue_size,
+            active_channels,
+            total_channels,
+            active_calls,
+            total_calls,
+            total_drops,
+        ),
+    )
 
 
 def update(db: sqlite3.Cursor, field: str, id: int, value):
@@ -25,9 +67,6 @@ class PhoneBooth:
         self.phone_service = phone_service
         self.num_phones = num_phones
         self.phone = simpy.Resource(env, num_phones)
-
-    def call(self, db: sqlite3.Cursor, person):
-        yield self.env.process(self.phone_service.call(db, person))
 
 
 class Person:
@@ -45,71 +84,92 @@ class Person:
         self.insufficient_funds = False
 
     def process(self, db: sqlite3.Cursor):
-        print(f"Person {self.id} arrives at the phone booth at {env.now:.2f}")
-        self.arrival = env.now
-        update(db, "arrival", self.id, self.arrival)
+        global queue_size
+
         with self.phone_booth.phone.request() as request:
             yield request
 
-            print(f"Person {self.id} enters the phone booth at {env.now:.2f}")
-            self.access = env.now
+            queue_size -= 1
+
+            printer(f"Person {self.id} enters the phone booth at {self.env.now:.2f}")
+            self.access = self.env.now
             update(db, "access", self.id, self.access)
+            time_series(db, self.env.now)
+
+            if self.call_time <= 0:
+                self.insufficient_funds = True
+                update(db, "insufficient_funds", self.id, self.insufficient_funds)
 
             while self.call_time > 0:
                 try:
-                    yield env.process(self.phone_booth.call(db, self))
+                    yield self.env.process(
+                        self.phone_booth.phone_service.call(db, self)
+                    )
                 except RuntimeError:
-                    print(f"Person {self.id} tries calling again at {env.now:.2f}")
-                except Exception:
-                    self.insufficient_funds = True
-                    update(db, "insufficient_funds", self.id, self.insufficient_funds)
-                    break
+                    printer(
+                        f"Person {self.id} tries calling again at {self.env.now:.2f}"
+                    )
+                    time_series(db, self.env.now)
 
 
 class PhoneService:
     def __init__(self, env: simpy.Environment):
         self.env = env
-        self.current_channels = 0
         self.channels = simpy.Resource(env, NUM_CHANNELS)
 
     def call(self, db: sqlite3.Cursor, person: Person):
+        global active_calls, total_drops, active_channels, total_channels, total_calls
         with self.channels.request() as _caller:
-            self.current_channels += 1
+            active_channels += 1
+            total_channels += 1
 
-            yield env.timeout(CALL_SETUP_TIME)
+            yield self.env.timeout(CALL_SETUP_TIME)
 
-            print(f"Person {person.id} tries calling at {env.now:.2f}")
-            person.call_start = env.now
+            printer(f"Person {person.id} tries calling at {self.env.now:.2f}")
+            person.call_start = self.env.now
             update(db, "call_start", person.id, person.call_start)
+            time_series(db, self.env.now)
 
-            if person.call_time <= 0:
-                raise Exception
-
-            if (
-                self.current_channels > CALL_DROP_AMOUNT
-                and random.random() > CALL_DROP_RATE
-            ):
+            if active_channels > CALL_DROP_AMOUNT and random.random() > CALL_DROP_RATE:
+                active_channels -= 1
+                total_drops += 1
                 raise RuntimeError
 
             with self.channels.request() as _called:
-                self.current_channels += 1
-                start = env.now
-                yield env.timeout(person.call_time)
+                active_channels += 1
+                total_channels += 1
+                active_calls += 1
+                total_calls += 1
+                start = self.env.now
+                yield self.env.timeout(person.call_time)
                 person.call_time = 0
-                end = env.now
-                print(
-                    f"Person {person.id} talked for {end - start:.2f} minutes at {env.now:.2f}"
+                end = self.env.now
+                printer(
+                    f"Person {person.id} talked for {end - start:.2f} minutes at {self.env.now:.2f}"
                 )
-            self.current_channels -= 1
-        self.current_channels -= 1
+                time_series(db, self.env.now)
+            active_channels -= 1
+            active_calls -= 1
+        active_channels -= 1
 
-        person.finished = env.now
+        person.finished = self.env.now
         update(db, "finished", person.id, person.finished)
+        time_series(db, self.env.now)
 
 
 def setup(env, db: sqlite3.Cursor, time_interval):
+    global queue_size
+
     for person in persons:
-        yield env.timeout(random.randint(time_interval - 8, time_interval + 8))
+        yield env.timeout(random.randint(time_interval - 2, time_interval + 2))
+
+        printer(f"Person {person.id} arrives at the phone booth at {env.now:.2f}")
+        person.arrival = env.now
+        update(db, "arrival", person.id, person.arrival)
+
+        queue_size += 1
+        time_series(db, env.now)
+
         env.process(person.process(db))
 
 
@@ -145,6 +205,10 @@ if __name__ == "__main__":
 
         cursor.execute(
             "CREATE TABLE person_statistics(id, start_funds, arrival, access, call_start, finished, insufficient_funds)"
+        )
+
+        cursor.execute(
+            "CREATE TABLE time_series(timestamp, queue_size, active_channels, total_channels, active_calls, total_calls, total_drops)"
         )
 
         cursor.executemany(
