9a9999aafb3972bea905327dc9af12d274a3606e
Hampus Avekvist <ha@leissner.se>
2024-10-31 11:04:15 +0100

    feat: generate graphs based on expected averages of queue length and time

--- phone_service/database_reader.py
+++ phone_service/database_reader.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 
     with sqlite3.connect("statistics.db", autocommit=True) as db:
         rates = pd.read_sql(
-            "SELECT num_phones, arrival, service FROM rates GROUP BY num_phones",
+            "SELECT num_phones, arrival, service, expected_waiting_time, expected_queue_length FROM rates GROUP BY num_phones",
             db,
         )
         averages = pd.read_sql(
@@ -38,6 +38,24 @@ if __name__ == "__main__":
     plt.ylabel("Service rate")
     plt.title("Service rate")
     plt.savefig("service.png", dpi=600)
+
+    expected_queue_length = rates[["num_phones", "expected_queue_length"]]
+    expected_queue_length.set_index("num_phones", inplace=True)
+    plt.figure()
+    expected_queue_length.plot()
+    plt.xlabel("Number of phones")
+    plt.ylabel("Queue length")
+    plt.title("Expected queue length")
+    plt.savefig("expected_queue_length.png", dpi=600)
+
+    expected_waiting_time = rates[["num_phones", "expected_waiting_time"]]
+    expected_waiting_time.set_index("num_phones", inplace=True)
+    plt.figure()
+    expected_waiting_time.plot()
+    plt.xlabel("Number of phones")
+    plt.ylabel("Waiting time")
+    plt.title("Expected waiting time")
+    plt.savefig("expected_waiting_time.png", dpi=600)
 
     print(averages)
 
