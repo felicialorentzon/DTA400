6b3e56e3fdc22cfaa6837e80979d85c05c012624
Hampus Avekvist <ha@leissner.se>
2024-11-06 13:44:31 +0100

    feat: generate graphs based on arrival rate

--- phone_service/database_reader.py
+++ phone_service/database_reader.py
@@ -1,8 +1,19 @@
+import sys
 import sqlite3
 import pandas as pd
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        print("Usage: python database_reader.py <arrival_rate>")
+        exit(1)
+
+    try:
+        arrival_rate = int(sys.argv[1])
+    except Exception:
+        print("Arrival rate must be an integer")
+        exit(1)
+
     plt.close("all")
 
     start_satisfaction = None
@@ -28,7 +39,7 @@ if __name__ == "__main__":
     plt.xlabel("Number of phones")
     plt.ylabel("Arrival rate")
     plt.title("Arrival rate")
-    plt.savefig("arrival.png", dpi=600)
+    plt.savefig(f"arrival-{arrival_rate}.png", dpi=600)
 
     service = rates[["num_phones", "service"]]
     service.set_index("num_phones", inplace=True)
@@ -37,7 +48,7 @@ if __name__ == "__main__":
     plt.xlabel("Number of phones")
     plt.ylabel("Service rate")
     plt.title("Service rate")
-    plt.savefig("service.png", dpi=600)
+    plt.savefig(f"service-{arrival_rate}.png", dpi=600)
 
     expected_queue_length = rates[["num_phones", "expected_queue_length"]]
     expected_queue_length.set_index("num_phones", inplace=True)
@@ -46,7 +57,7 @@ if __name__ == "__main__":
     plt.xlabel("Number of phones")
     plt.ylabel("Queue length")
     plt.title("Expected queue length")
-    plt.savefig("expected_queue_length.png", dpi=600)
+    plt.savefig(f"expected_queue_length-{arrival_rate}.png", dpi=600)
 
     expected_waiting_time = rates[["num_phones", "expected_waiting_time"]]
     expected_waiting_time.set_index("num_phones", inplace=True)
@@ -55,7 +66,7 @@ if __name__ == "__main__":
     plt.xlabel("Number of phones")
     plt.ylabel("Waiting time")
     plt.title("Expected waiting time")
-    plt.savefig("expected_waiting_time.png", dpi=600)
+    plt.savefig(f"expected_waiting_time-{arrival_rate}.png", dpi=600)
 
     print(averages)
 
@@ -66,7 +77,7 @@ if __name__ == "__main__":
     plt.xlabel("Number of phones")
     plt.ylabel("People in queue")
     plt.title("Average queue size upon arrival")
-    plt.savefig("queue_size.png", dpi=600)
+    plt.savefig(f"queue_size-{arrival_rate}.png", dpi=600)
 
     queue_time = averages[["num_phones", "queue_time"]]
     queue_time.set_index("num_phones", inplace=True)
@@ -75,7 +86,7 @@ if __name__ == "__main__":
     plt.xlabel("Number of phones")
     plt.ylabel("Time in queue")
     plt.title("Average queue time upon exit")
-    plt.savefig("queue_time.png", dpi=600)
+    plt.savefig(f"queue_time-{arrival_rate}.png", dpi=600)
 
     satisfaction = averages[["num_phones", "satisfaction"]]
     satisfaction.set_index("num_phones", inplace=True)
@@ -84,4 +95,4 @@ if __name__ == "__main__":
     plt.xlabel("Number of phones")
     plt.ylabel("Satisfaction")
     plt.title("Average satisfaction")
-    plt.savefig("satisfaction.png", dpi=600)
+    plt.savefig(f"satisfaction-{arrival_rate}.png", dpi=600)
